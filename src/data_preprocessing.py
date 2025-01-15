64551a4bcfdb858fe45370b04929af3b06d5cb81
ghrsmg <gherasimgabriel309@gmail.com>
2025-01-15 20:32:37 +0200

    data file was too big to push on git so I put a different version with a smaller dataset

--- src/data_preprocessing.py
+++
@@ -1,9 +0,0 @@
-
-def preprocess_data(data):
-    """
-    Resample to daily data and clean the Bitcoin dataset for analysis.
-    """
-    data = data.set_index('Timestamp')
-    daily_data = data.resample('D').mean()
-    daily_data = daily_data.dropna()
-    return daily_data
