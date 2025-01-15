d404abf70cb78f61c0e239331e94fe185f3110ce
ghrsmg <gherasimgabriel309@gmail.com>
2025-01-15 19:07:27 +0200

    last touch on the project

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,10 +1,8 @@
-import pandas as pd
 
 def preprocess_data(data):
     """
-    Resample and clean the Bitcoin dataset for analysis.
+    Resample to daily data and clean the Bitcoin dataset for analysis.
     """
-    # Resample to daily data for aggregation
     data = data.set_index('Timestamp')
     daily_data = data.resample('D').mean()
     daily_data = daily_data.dropna()
